328faba3bdddb4b134b4a48382801f688d78f444
blitzdawolf <botjes2@hotmail.com>
2022-07-16 00:05:45 +0200

    Added basic webserver to dynamic server

---
+++ DynamicServer/WebServer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DynamicServer
+{
+    public class WebServer : BackgroundService
+    {
+        public WebServer()
+        {
+
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Delay(1000);
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 1302);
+            listener.Start();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+
+                try
+                {
+                    string request = sr.ReadLine();
+                    Console.WriteLine(request);
+                    string[] tokens = request.Split(' ');
+
+                    string page = tokens[1];
+                    if (page == "/")
+                    {
+
+                    }
+
+                    sw.WriteLine("HTTP/1.0 200 OK\n");
+                    sw.WriteLine("{\"Test\": 5}");
+                    sw.Flush();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                client.Close();
+            }
+        }
+    }
+}
--- TestEventServer/Program.cs
+++ TestEventServer/Program.cs
@@ -1,14 +1,19 @@
+using DynamicServer;
 using MainServer;
 using TestEventServer;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    /*.ConfigureServices(services =>
     {
         services.AddSingleton<Network>();
         services.AddSingleton<ReciveHandler>();
         services.AddSingleton<ServerHandler>();
 
         services.AddHostedService<CustomServer>();
+    })*/
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<WebServer>();
     })
     .Build();
 
